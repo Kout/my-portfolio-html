8dcec73f1d1abf11718f2f4198e120154c8b5ff7
Kout <kout.tk@gmail.com>
2018-01-30 20:37:10 +0100

    Add new task in gulpfile to copy JS files

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,12 @@ gulp.task('html:copy', function() {
 		.pipe(gulp.dest('dist/'));
 });
 
+// Copy all JS files
+gulp.task('js:copy', function() {
+	return gulp.src('src/**/*.js')
+		.pipe(gulp.dest('dist/'));
+});
+
 // Delete all static files such as images etc.
 gulp.task('static:clean', function() {
 	return del([
@@ -40,12 +46,13 @@ gulp.task('static:copy', ['static:clean'], function() {
 			.pipe(gulp.dest('dist/img/'));
 });
 
-gulp.task('build', ['css:compile', 'html:copy', 'static:copy']);
+gulp.task('build', ['css:compile', 'html:copy', 'static:copy', 'js:copy']);
 
 gulp.task('develop', ['build'], function() {
 	gulp.watch('src/scss/*', ['css:compile']); // watch for changes in SCSS
 	gulp.watch('src/**/*.html', ['html:copy']); // watch for changes in HTML
 	gulp.watch('src/img/**/*', ['static:copy']); // watch for changes in static files
+	gulp.watch('src/js/**/*', ['js:copy']); // watch for changes in javascript files
 });
 
 // Define default task that can be called by just running `gulp` from cli
